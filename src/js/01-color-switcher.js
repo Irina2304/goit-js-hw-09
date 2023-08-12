cda2e9a7751bef584864b2a33972f4908b5cbafe
Irina2304 <110673165+Irina2304@users.noreply.github.com>
2023-08-12 19:34:19 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const page = document.querySelector("body")
+const start = document.querySelector("button[data-start]")
+const stop = document.querySelector("button[data-stop]")
+
+let timerId;
+
+start.addEventListener("click", onStart);
+stop.addEventListener("click", onStop);
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function onStart() {
+    timerId = setInterval(() => {
+        const color = getRandomHexColor();
+        page.style.backgroundColor = color
+    }, 1000);
+    start.setAttribute('disabled', "");
+}
+
+function onStop() {
+    clearInterval(timerId);
+    start.removeAttribute('disabled');
+}
 
